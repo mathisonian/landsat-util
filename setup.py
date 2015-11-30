7f58afd7df7b42a79b0e4be31cc87210e4439e1d
Matthew Conlen <mc@mathisonian.com>
2015-12-01 01:49:21 +0800

    versions

--- setup.py
+++ setup.py
@@ -37,11 +37,11 @@ setup(
     install_requires=[
       'requests==2.7.0',
       'python-dateutil>=2.4.2',
-      'numpy>=1.9.3',
+      'numpy',
       'termcolor>=1.1.0',
       'rasterio>=0.26.0',
       'six==1.9.0',
-      'scipy>=0.16.0',
+      'scipy',
       'scikit-image>=0.11.3',
       'homura>=0.1.2',
       'boto>=2.38.0',
